1c5388fa5e19eeaa780ddc26705d4ca1894040e5
Cleiton Luiz <cleiton.vanquish@gmail.com>
2019-09-16 20:25:04 -0300

    Add resume file. Create contact section

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ router.get("/", function (req, res) {
 
 app.use("/", router);
 
-app.use("*", function (req, res) {
-    res.sendFile(path + "404.html");
-});
-
 app.listen(3000, function () {
     console.log("Live at Port 3000");
 });
